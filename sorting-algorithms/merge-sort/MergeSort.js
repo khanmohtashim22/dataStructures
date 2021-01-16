24855533b75595bee0727c5e440a0a7e779d811c
Mohtashim Khan <khanmohtashim22@gmail.com>
2021-01-15 22:23:33 -0500

    merge sort

---
+++ sorting-algorithms/merge-sort/MergeSort.js
@@ -0,0 +1,44 @@
+/*
+
+Merge Sort
+
+Time Complexity
+O(n log n)
+
+*/
+
+// Merge function from earlier
+function merge(arr1, arr2) {
+  let results = []
+  let i = 0
+  let j = 0
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i])
+      i++
+    } else {
+      results.push(arr2[j])
+      j++
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i])
+    i++
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j])
+    j++
+  }
+  return results
+}
+
+// Recrusive Merge Sort
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr
+  let mid = Math.floor(arr.length / 2)
+  let left = mergeSort(arr.slice(0, mid))
+  let right = mergeSort(arr.slice(mid))
+  return merge(left, right)
+}
+
+console.log(mergeSort([100,2,3,123,345,2,657,3,4,6,9,1]))
